8673afcda32a772696e956b91cdccebf5defada5
Fran Sanchez <fransan119@gmail.com>
2020-06-05 09:22:47 +0200

    nested class

--- src/test/java/com/fns/soaptemplate/country/CountryClientTest.java
+++ src/test/java/com/fns/soaptemplate/country/CountryClientTest.java
@@ -2,6 +2,7 @@ package com.fns.soaptemplate.country;
 
 import com.fns.soaptemplate.country.client.CountryClient;
 import com.fns.soaptemplate.country.client.gen.GetCountryResponse;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,15 +15,18 @@ class CountryClientTest {
     @Autowired
     CountryClient client;
 
-    @Test
-    public void givenCountryService_whenCountrySpain_thenCapitalIsMadrid() {
-        GetCountryResponse response = client.getCountry("Spain");
-        assertEquals("Madrid", response.getCountry().getCapital());
-    }
+    @Nested
+    public class countryService {
+        @Test
+        public void whenCountrySpain_thenCapitalIsMadrid() {
+            GetCountryResponse response = client.getCountry("Spain");
+            assertEquals("Madrid", response.getCountry().getCapital());
+        }
 
-    @Test
-    public void givenCountryService_whenCountryUk_thenCapitalIsLondon() {
-        GetCountryResponse response = client.getCountry("UK");
-        assertEquals("London", response.getCountry().getCapital());
+        @Test
+        public void whenCountryUk_thenCapitalIsLondon() {
+            GetCountryResponse response = client.getCountry("UK");
+            assertEquals("London", response.getCountry().getCapital());
+        }
     }
 }
